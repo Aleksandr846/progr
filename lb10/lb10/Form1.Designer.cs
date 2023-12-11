4f940b7d2802ff906bb66012ce8038945d4a8fa9
Aleksandr <norama8282@ksyhtc.com>
2023-12-11 22:13:51 +0300

    addlb10

---
+++ lb10/lb10/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace lb10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelname = new System.Windows.Forms.Label();
+            this.textBoxname = new System.Windows.Forms.TextBox();
+            this.labelphonenumber = new System.Windows.Forms.Label();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.Location = new System.Drawing.Point(12, 9);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(79, 13);
+            this.labelname.TabIndex = 0;
+            this.labelname.Text = "Введите ФИО";
+            this.labelname.Click += new System.EventHandler(this.labelname_Click);
+            // 
+            // textBoxname
+            // 
+            this.textBoxname.Location = new System.Drawing.Point(97, 6);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(184, 20);
+            this.textBoxname.TabIndex = 1;
+            this.textBoxname.TextChanged += new System.EventHandler(this.textBoxname_TextChanged);
+            // 
+            // labelphonenumber
+            // 
+            this.labelphonenumber.AutoSize = true;
+            this.labelphonenumber.Location = new System.Drawing.Point(12, 43);
+            this.labelphonenumber.Name = "labelphonenumber";
+            this.labelphonenumber.Size = new System.Drawing.Size(104, 13);
+            this.labelphonenumber.TabIndex = 2;
+            this.labelphonenumber.Text = "Введите номер тел";
+            this.labelphonenumber.Click += new System.EventHandler(this.labelphonenumber_Click);
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(122, 43);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(159, 20);
+            this.textBoxPhoneNumber.TabIndex = 3;
+            this.textBoxPhoneNumber.TextChanged += new System.EventHandler(this.textBoxPhoneNumber_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(15, 284);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.Location = new System.Drawing.Point(12, 96);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(258, 134);
+            this.listBoxContacts.TabIndex = 5;
+            this.listBoxContacts.SelectedIndexChanged += new System.EventHandler(this.listBoxContacts_SelectedIndexChanged);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(206, 284);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 6;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 319);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.listBoxContacts);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.labelphonenumber);
+            this.Controls.Add(this.textBoxname);
+            this.Controls.Add(this.labelname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.TextBox textBoxname;
+        private System.Windows.Forms.Label labelphonenumber;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBoxContacts;
+        private System.Windows.Forms.Button buttonRemove;
+    }
+}
+
---
+++ lb10/lb10/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb10
+{
+    public partial class Form1 : Form
+    {
+        private const string filePath = "contacts.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadContacts();
+        }
+        private void LoadContacts()
+        {
+            listBoxContacts.Items.Clear();
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                listBoxContacts.Items.AddRange(lines);
+            }
+        }
+
+        private void SaveContacts()
+        {
+            File.WriteAllLines(filePath, listBoxContacts.Items.Cast<string>());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelphonenumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPhoneNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxContacts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedIndex != -1)
+            {
+                string selectedContact = listBoxContacts.SelectedItem.ToString();
+                string[] parts = selectedContact.Split(':');
+                if (parts.Length == 2)
+                {
+                    textBoxname.Text = parts[0].Trim();
+                    textBoxPhoneNumber.Text = parts[1].Trim();
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string contact = $"{textBoxname.Text}: {textBoxPhoneNumber.Text}";
+            listBoxContacts.Items.Add(contact);
+            SaveContacts();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedIndex != -1)
+            {
+                listBoxContacts.Items.RemoveAt(listBoxContacts.SelectedIndex);
+                SaveContacts();
+            }
+        }
+    }
+}
