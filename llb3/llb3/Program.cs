ae552721b4668bb885619eb41c4e43258694f16c
Aleksandr <norama8282@ksyhtc.com>
2023-12-11 22:20:38 +0300

    addlb3

--- llb3/llb3/Program.cs
+++ llb3/llb3/Program.cs
@@ -1,253 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace lb88
+namespace llb3
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
-
-        public Point()
+        int x;
+        int y;
+        string sym = "-----";
+        public void SetX(int x)
         {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
         }
-        public Point(int x, int y, char sym)
+        public void SetY(int y)
         {
-            this.x = x;
-            this.y = y;
-            this.sym = sym;
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
         }
-        public Point(Point p)
-        {
-            x = p.x;
-            y = p.y;
-            sym = p.sym;
-        }
-        public void Draw()
+        public void Draw(string sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-        }
-        public bool IsHit(Point p)
-        {
-            return p.x == this.x && p.y == this.y;
-        }
-        public void Move(int offset, Direction direction)
-        {
-            if (direction == Direction.RIGHT)
-            {
-                x = x + offset;
-            }
-            else if (direction == Direction.LEFT)
-            {
-                x = x - offset;
-            }
-            else if (direction == Direction.UP)
-            {
-                y = y - offset;
-            }
-            else if (direction == Direction.DOWN)
-            {
-                y = y + offset;
-            }
-        }
-        public void Clear()
-        {
-            sym = ' ';
-            Draw();
+            y = y + 1;
         }
     }
-    class Figure
+    class Program
     {
-        protected List<Point> pList;
-
-        public void Draw()
+        static void Main(string[] args)
         {
-            foreach (Point p in pList)
+            Point a = new Point();
+            Console.Write("Введите координату x: ");
+            try
             {
-                p.Draw();
+                a.SetX(Convert.ToInt32(Console.ReadLine()));
             }
-        }
-
-        public bool IsHit(Figure figure)
-        {
-            foreach (var p in pList)
+            catch (System.Exception)
             {
-                if (figure.IsHit(p))
-                    return true;
+                Console.WriteLine("Значение x не может быть отрицательным");
+                Console.WriteLine("Введите координату x: ");
+                a.SetX(Convert.ToInt32(Console.ReadLine()));
             }
-            return false;
-        }
-
-        private bool IsHit(Point point)
-        {
-            foreach (var p in pList)
+            Console.Write("Введите координату y: ");
+            try
             {
-                if (p.IsHit(point))
-                    return true;
+                a.SetY(Convert.ToInt32(Console.ReadLine()));
             }
-            return false;
-        }
-    }
-    class VerticalLine : Figure
-    {
-        public VerticalLine(int yUp, int yDown, int x, char sym)
-        {
-            pList = new List<Point>();
-            for (int y = yUp; y <= yDown; y++)
+            catch (System.Exception)
             {
-                Point p = new Point(x, y, sym);
-                pList.Add(p);
+                Console.WriteLine("Значение y не может быть отрицательным ");
+                Console.WriteLine("Введите координату y: ");
+                a.SetY(Convert.ToInt32(Console.ReadLine()));
             }
-        }
-    }
-
-    class HorizontalLine : Figure
-    {
-        public HorizontalLine(int xLeft, int xRight, int y, char sym)
-        {
-            pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
-            {
-                Point p = new Point(x, y, sym);
-                pList.Add(p);
-            }
-        }
-    }
-    enum Direction
-    {
-        LEFT,
-        RIGHT,
-        UP,
-        DOWN
-    }
-    class Snake : Figure
-    {
-        public static byte gg = 0;
-        Direction direction;
-
-        public Snake(Point tail, int length, Direction _direction)
-        {
-            direction = _direction;
-            pList = new List<Point>();
-            for (int i = 0; i < length; i++)
-            {
-                Point p = new Point(tail);
-                p.Move(i, direction);
-                pList.Add(p);
-            }
-        }
-        public void Move()
-        {
-            Point tail = pList.First();
-            pList.Remove(tail);
-            Point head = GetNextPoint();
-            pList.Add(head);
-            tail.Clear();
-            head.Draw();
-        }
-        public Point GetNextPoint()
-        {
-            Point head = pList.Last();
-            Point nextPoint = new Point(head);
-            nextPoint.Move(1, direction);
-            return nextPoint;
-        }
-        public void HandleKey(ConsoleKey key)
-        {
-            if (key == ConsoleKey.LeftArrow)
-                direction = Direction.LEFT;
-            else if (key == ConsoleKey.RightArrow)
-                direction = Direction.RIGHT;
-            else if (key == ConsoleKey.DownArrow)
-                direction = Direction.DOWN;
-            else if (key == ConsoleKey.UpArrow)
-                direction = Direction.UP;
-        }
-        public bool Eat(Point food)
-        {
-            Point head = GetNextPoint();
-            if (head.IsHit(food))
-            {
-                food.sym = head.sym;
-                pList.Add(food);
-                return true;
-            }
-            else
-                return false;
-        }
-        public bool SnakeHit()
-        {
-            foreach (var p in pList)
-            {
-                Point head = GetNextPoint();
-                if (p.IsHit(head))
-                    return true;
-            }
-            return false;
-        }
-        class Program
-        {
-            static void Main(string[] args)
-            {
-                Walls walls = new Walls(120, 30);
-                walls.Draw();
-
-
-                Point p = new Point(3, 5, '*');
-                Snake snake = new Snake(p, 4, Direction.RIGHT);
-                snake.Draw();
-
-                Point food = new Point(4, 2, '*');
-                food.Draw();
-                Random rand = new Random();
-                int randomvalueX = rand.Next(5, 115);
-                int randomvalueY = rand.Next(5, 25);
-                while (true)
-                {
-                    if (snake.SnakeHit() == true)
-                    {
-                        Console.Write("Вы врезались в хвост");
-                        Console.ReadKey();
-                        break;
-                    }
-                    if (walls.IsHit(snake) == true)
-                    {
-                        Console.Write("Вы врезались в стену");
-                        Console.ReadKey();
-                        break;
-                    }
-                    if (snake.Eat(food))
-                    {
-
-                        randomvalueX = rand.Next(5, 115);
-                        randomvalueY = rand.Next(5, 25);
-
-                        food = new Point(randomvalueX, randomvalueY, '*');
-                        food.Draw();
-
-                        Snake.gg++;
-
-                        if (Snake.gg == 10)
-                        {
-                            Console.WriteLine("Вы выиграли");
-                        }
-                    }
-                    else
-                    {
-                        snake.Move();
-                    }
-                    Thread.Sleep(150);
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo key = Console.ReadKey();
-                        snake.HandleKey(key.Key);
-                    }
-                }
-            }
+            a.Draw("()");
+            a.Draw("||");
+            a.Draw("/\\");
+            Console.WriteLine("\nНажмите любую клавишу, чтобы завершить программу...");
+            Console.ReadKey();
         }
     }
 }
